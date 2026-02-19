e0eabc8125534b12caa37aafbf73af197a69d8bf
Captain Chauhan <coderchauhan2005@gmail.com>
2026-02-19 20:32:19 +0530

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel,EmailStr,Field
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str 
+    is_verfied: Optional[bool] = False
+
+class OTPVerify(BaseModel):
+    email: EmailStr
+    otp: str = Field(..., min_length=6, max_length=6)
+
+class WorkerCreate(BaseModel):
+    name: str
+    mobile_number: str
+    adhar: str
+    working_time: str
+    working_days: str
+    salary: int
+
+class WorkerOut(WorkerCreate):
+    id: int
+
+    class Config:
+        from_attributes= True

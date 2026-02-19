f080f5666c48bb50b6174ad4d95936ea20a450a3
Captain Chauhan <coderchauhan2005@gmail.com>
2026-02-19 20:37:00 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,10 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+import os
+
+pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = os.getenv("SECRET_KEY", "your_secrate_key")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
